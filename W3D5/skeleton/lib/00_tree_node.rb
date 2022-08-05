6c9e49b68eeb8d00b7fcc9e3b3f9d3db8dc254c2
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-08-05 13:31:01 -0400

    add parent=

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(new_parent)
+        if self.parent != nil
+            self.parent.children.each_with_index 
+        end
+
+    end
+
 
 end
